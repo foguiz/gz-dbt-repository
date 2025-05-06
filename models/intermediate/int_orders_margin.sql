9edb3bf5e5fb8711651d90818f2b4982f3ac281a
victor.lindgrenb@gmail.com <victor.lindgrenb@gmail.com>
2025-05-06 14:21:14 +0000

    int_orders_margin commit

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,7 @@
+select
+    orders_id
+    ,round(sum(revenue),2) order_revenue
+    ,round(sum(purchase_cost),2) order_purchase_cost
+    ,round(sum(margin),2) order_margin
+from {{ref('int_sales_margin')}}
+group by orders_id
