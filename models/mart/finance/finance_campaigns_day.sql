3ceca31c3f942f5deff167e949f86f352feaacb1
victor.lindgrenb@gmail.com <victor.lindgrenb@gmail.com>
2025-05-07 14:14:33 +0000

    documentation and tests

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -5,15 +5,17 @@ campaign as (select * from {{ref("int_campaigns_day")}})
 select
     campaign.date_date
     ,round(finance.operational_margin - campaign.ads_cost, 2) ads_margin
+    ,finance.average_basket
+    ,finance.operational_margin
     ,campaign.ads_cost
     ,campaign.ads_impressions
     ,campaign.ads_clicks
-    ,finance.nb_products_total
+    ,finance.nb_products_total quantity
     ,finance.revenue
-    ,finance.total_purchase_cost
+    ,finance.total_purchase_cost purchase_cost
     ,finance.margin
-    ,finance.total_shipping_fees
-    ,finance.total_log_costs
+    ,finance.total_shipping_fees shipping_fee
+    ,finance.total_log_costs logcost
     ,finance.ship_cost
 from campaign
 inner join finance using (date_date)
